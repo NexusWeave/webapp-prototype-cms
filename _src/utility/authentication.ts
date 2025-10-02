e96b4e52fe42c521358cbb1e2598c0fd2c3f9888
krigjo25 <krigjo25@gmail.com>
2025-10-02 13:25:37 +0200

    feat(authentication) : implementing JWT Claims
        -   Implementing a new utility function responsible for securely decoding JSON Web Tokens. This enables the application to read and access embedded claims which are required for authentication and authorization logic

---
+++ _src/utility/authentication.ts
@@ -0,0 +1,34 @@
+//  Imports and interface Logic
+
+interface JWTPayLoadClaims
+{
+    iss: string;
+    aud: string;
+    iat: number;
+    nbf: number;
+    exp: number;
+    sub: string;
+    scope?: Array<string>;
+}
+
+function Base64Url(token: string): JWTPayLoadClaims
+{
+    // Example decoder
+
+    const payload = token.split('.')[1];
+    if (!payload) throw new Error('Invalid token');
+
+    const safePayload = payload.replace(/-/g, '+').replace(/_/g, '/');
+    const paddedPayload = safePayload + '==='.slice(0, (4 - safePayload.length % 4) % 4);
+
+    const raw = window.atob(paddedPayload);
+
+    const utf8 = decodeURIComponent(
+        raw.split('').map(function(c)
+    {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+
+    return JSON.parse(utf8) as JWTPayLoadClaims;
+
+}
